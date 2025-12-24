27b5c8e7e7618fcb7e2a879307277b0454e11d0c
rootchzt13 <parischzt@gmail.com>
2025-12-24 17:20:19 +0700

    Fix: Resolve Supabase identifier redeclaration error

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const SUPABASE_URL = 'https://jadljxucecpvpkfcgxyl.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphZGxqeHVjZWNwdnBrZmNneHlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3MzYwMDgsImV4cCI6MjA2ODMxMjAwOH0.TBlI1rbu9gW7mEBJiru47hBYBsJJDc-OVRSd26OUNWU';
 
 const { createClient } = window.supabase;
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
